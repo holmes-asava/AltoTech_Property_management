690121fb4c2f79385ef1d2857a56db9c5b3cb486
Holme.A <homer.asava@gmail.com>
2023-05-27 02:34:41 +0700

    create permission

---
+++ config/permissions.py
@@ -0,0 +1,33 @@
+from rest_framework.permissions import BasePermission
+
+__all__ = [
+    "IsAuthenticated",
+    "IsMaid",
+    "IsSupervisor",
+]
+
+
+class IsAuthenticated(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user and request.user.is_active and request.user.is_authenticated
+        )
+
+
+class IsSuperuser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user and request.user.is_active and request.user.is_superuser
+        )
+
+
+class IsMaid(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_active and request.user.is_maid)
+
+
+class IsSupervisor(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user and request.user.is_active and request.user.is_supervisor
+        )
