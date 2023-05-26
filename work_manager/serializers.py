34089cbdfdb4746e9793644894ea1823fe8d2ce6
Holme.A <homer.asava@gmail.com>
2023-05-27 04:00:53 +0700

    add validation in serializer

--- work_manager/serializers.py
+++ work_manager/serializers.py
@@ -2,10 +2,16 @@ from django.core.exceptions import ValidationError
 
 from rest_framework import serializers
 
-from work_manager.models import WorkOrder
+from work_manager.models import WorkOrder, TechnicianRequest
 
 
 class WorkOrderSerializer(serializers.ModelSerializer):
+    amenity_request_list = serializers.JSONField(required=False)
+    description = serializers.CharField(required=False)
+    defect_type = serializers.ChoiceField(
+        choices=TechnicianRequest.DefectType.choices, required=False
+    )
+
     class Meta:
         model = WorkOrder
 
@@ -16,10 +22,31 @@ class WorkOrderSerializer(serializers.ModelSerializer):
             "finished_at",
             "work_type",
             "work_status",
+            "amenity_request_list",
+            "description",
+            "defect_type",
         ]
         read_only = [
             "work_order_number",
         ]
+
+    def validate(self, data):
+        amenity_request_list = data.get("amenity_request_list")
+        description = data.get("description")
+        defect_type = data.get("defect_type")
+        work_type = data.get("work_type")
+        if amenity_request_list and work_type != WorkOrder.WorkType.AMENITY_REQUEST:
+            raise serializers.ValidationError(
+                "This WorkOrder type doesn't support amenity_request_list field"
+            )
+        if description and work_type != WorkOrder.WorkType.MAID_REQUEST:
+            raise serializers.ValidationError(
+                "This WorkOrder type doesn't support description field"
+            )
+        if defect_type and work_type != WorkOrder.WorkType.TECHNICIAN_REQUEST:
+            raise serializers.ValidationError(
+                "This WorkOrder type doesn't support defect_type field"
+            )
 
     def create(self, validated_data):
         request = self.context.get("request")
@@ -27,4 +54,20 @@ class WorkOrderSerializer(serializers.ModelSerializer):
         try:
             return super().create(validated_data)
         except ValidationError as e:
-            raise serializers.ValidationError(e.message)
+            detail = getattr(e, "message", None) or str(e)
+            raise serializers.ValidationError(detail)
+
+
+class UpdateWorkOrderSerializer(WorkOrderSerializer):
+    class Meta:
+        model = WorkOrder
+
+        fields = [
+            "assigned_to",
+            "room",
+            "finished_at",
+            "work_status",
+            "amenity_request_list",
+            "description",
+            "defect_type",
+        ]
